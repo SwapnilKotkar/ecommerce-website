3763f420bffac1098a32e187f867658d60d12f4d
swapnil <116189264+viztech-systems@users.noreply.github.com>
2022-10-22 00:06:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SignUp from './pages/SignUp'
+import Signin from './pages/Signin'
+import Home from "./pages/Home";
+import Errorpage from "./pages/Errorpage";
+import ProductDetails from "./components/ProductDetails";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+        <Routes>
+              <Route exact path='/' element={<Home/>} />  
+              <Route exact path='/signup' element={<SignUp/>} />
+              <Route exact path='/signin' element={<Signin/>} />
+              <Route exact path='/productdetails/:id' element={<ProductDetails/>} />
+              <Route path='*' element={<Errorpage/>} />
+        </Routes>
+      <Footer/>
+    </>
   );
 }
 
